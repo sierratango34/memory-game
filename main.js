deae67c7d567dd2453a7fb9df741065141004001
sierratango34 <k.sritawan@gmail.com>
2021-01-27 21:19:15 +0000

    basic memory game done, cards stick if match else cover up

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
+let clickedCard = null;
 
 function onCardClick(e) {
-  let clickedCard1 = null;
   const target = e.currentTarget;
   color = target.getAttribute('data-color')
 
+  if (target.className.includes('done')) {
+    return;
+  }
+
   target.className = target.className.replace('color-hidden', '').trim();
+  target.className += ' done'
 
-  if (!clickedCard1) {
+  if (!clickedCard) {
     clickedCard = target;
-  };
+  } else if (clickedCard) {
 
-
-
+    if (clickedCard.getAttribute('data-color') !== color) {
+      setTimeout(() => {
+        clickedCard.className = clickedCard.className.replace('done', '').trim() + ' color-hidden'
+        target.className = target.className.replace('done', '').trim() + ' color-hidden';
+        clickedCard = null;
+      }, 500);
+    } else {
+      clickedCard = null;
+    }
+  }
 }
